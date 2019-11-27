b92fe710b67c161d90e3eedf2f19cc509dedf320
pschake34 <pschake34@charlottesvilleschools.org>
2019-11-27 12:17:48 -0500

    Initial Commit

---
+++ src/cryptography/Cryptography.java
@@ -0,0 +1,197 @@
+package cryptography;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.xml.bind.DatatypeConverter;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+import java.io.*;
+
+
+public class Cryptography {
+    static Scanner sc = new Scanner(System.in);
+    static Random generator = new Random();
+    static String path = "/home/user/Desktop/Programming/Java/Cryptography/src/cryptography/KeyAliases.txt";
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Welcome to the Encryption Program");
+        while (true) {
+            System.out.println("1. Encrypt text \n 2. Decrypt text \n\n Enter Selection : ");
+            String option = sc.nextLine();
+            if (option.equals("1")) {
+                SecretKey key = generateKey();
+                String password = getHash(getPassword());
+                storeKey(key, password);
+                String toEncrypt = getPlainText();
+                byte[] encrypted = encrypt(toEncrypt, key);
+                String hexKey = bytesToHex(key.getEncoded());
+                System.out.println("Secret Key : " + hexKey + "\nEncypted Text : " + bytesToHex(encrypted));
+                System.out.println("\n\n1. Encrypt/Decrypt another message\n2. Exit\n\nEnter Selection : ");
+                option = sc.nextLine();
+                if (option.equals("2")) {
+                    System.out.println("Exiting...");
+                    System.exit(0);
+                }
+                else if (option.equals("1")){
+                    System.out.println("Redirecting...");
+                }
+                else {
+                    System.out.println("Invalid Option!\nRedirecting...");
+                }
+            } else if (option.equals("2")) {
+                System.out.println("Enter the Encrypted text : ");
+                byte[] encryptedText = hexToBytes(sc.nextLine());
+                SecretKey key = getKey();
+                String plainText = decrypt(encryptedText, key);
+                System.out.println("Decrypted Text : " + plainText);
+                System.out.println("\n\n1. Encrypt/Decrypt another message\n2. Exit\n\nEnter Selection : ");
+                option = sc.nextLine();
+                if (option.equals("2")) {
+                    System.out.println("Exiting...");
+                    System.exit(0);
+                }
+                else if (option.equals("1")){
+                    System.out.println("Redirecting...");
+                }
+                else {
+                    System.out.println("Invalid Option!\nRedirecting...");
+                }
+            }
+        }
+    }
+
+    public static SecretKey generateKey() throws Exception {
+        KeyGenerator generator = KeyGenerator.getInstance("AES");
+        generator.init(256);
+        SecretKey secret = generator.generateKey();
+        return secret;
+    }
+
+    public static String getPlainText() {
+        System.out.println("Enter the text you wish to Encrypt : ");
+        String plainText = sc.nextLine();
+        return plainText;
+    }
+
+    public static byte[] encrypt(String plainText, SecretKey secKey) throws Exception {
+        Cipher aesCipher = Cipher.getInstance("AES");
+        aesCipher.init(Cipher.ENCRYPT_MODE, secKey);
+        byte[] byteCipherText = aesCipher.doFinal(plainText.getBytes());
+        return byteCipherText;
+    }
+
+    public static String decrypt(byte[] encryptedText, SecretKey secKey) throws Exception {
+        Cipher aesCipher = Cipher.getInstance("AES");
+        aesCipher.init(Cipher.DECRYPT_MODE, secKey);
+        byte[] bytePlainText = aesCipher.doFinal(encryptedText);
+        return new String(bytePlainText);
+    }
+
+    private static String bytesToHex(byte[] hash) {
+        return DatatypeConverter.printHexBinary(hash);
+    }
+
+    private static byte[] hexToBytes(String hex){
+        byte[] bytes = new byte[hex.length() / 2];
+        for (int i = 0; i < bytes.length; i++) {
+            bytes[i] = (byte) Integer.parseInt(hex.substring(2*i, 2*i+2), 16);
+        }
+        return bytes;
+    }
+
+    public static String  getPassword(){
+        while (true) {
+            System.out.println("Enter the password for the key generation : ");
+            String input1 = sc.nextLine();
+            System.out.println("Enter the same password for confirmation : ");
+            String input2 = sc.nextLine();
+            if (input1.equals(input2)) {
+                if (input1.length() < 8) {
+                    System.out.println("Enter a longer password.");
+                } else {
+                    System.out.println("Password Confirmed");
+                    return input1;
+                }
+            } else {
+                System.out.println("Passwords did not match. Try Again.\n");
+            }
+        }
+    }
+
+    public static void storeKey(SecretKey key, String passwordHash) throws IOException {
+        String entryAlias = ".r" + generateString(30);
+        File file = new File(entryAlias);
+        ObjectOutputStream oout = new ObjectOutputStream(new FileOutputStream(entryAlias));
+        try {
+            oout.writeObject(key);
+        } finally {
+            oout.close();
+        }
+        BufferedWriter writer = new BufferedWriter(new FileWriter(path));
+        writer.write(entryAlias);
+        writer.newLine();
+        writer.write(passwordHash);
+        writer.close();
+
+    }
+
+    public static String generateString(int length) {
+        char[] characters = {'q', 'w', 'e', 'r', 't', 'y', 'u',
+                'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j',
+                'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm', '1', '2',
+                '3', '4', '5', '6', '7', '8', '9', '0', ' ', 'k'};
+        char[] string = new char[length];
+        for (int i = 0; i < length; i++) {
+            string[i] = characters[generator.nextInt(characters.length)];
+        }
+        return new String(string);
+    }
+
+    public static SecretKey getKey() throws IOException, ClassNotFoundException, NoSuchAlgorithmException{
+        System.out.println("Enter the password to the SecretKey that encrypted this message : ");
+        String password = getHash(sc.nextLine());
+        BufferedReader br = new BufferedReader(new FileReader(path));
+        ArrayList<String> passesKeys = new ArrayList<>();
+        try {
+            String line;
+            while ((line = br.readLine()) != null) {
+                passesKeys.add(line);
+            }
+
+        } finally {
+            br.close();
+        }
+        int index = findString(passesKeys, password);
+        String entryAlias = passesKeys.get(index);
+        FileInputStream fis = new FileInputStream(entryAlias);
+        ObjectInputStream ois = new ObjectInputStream(fis);
+        SecretKey secKey = (SecretKey) ois.readObject();
+        ois.close();
+        fis.close();
+        return secKey;
+    }
+
+    public static int findString(ArrayList<String> array , String toFindHash){
+        for (int i = 0; i < array.size(); i++){
+            if (toFindHash.equals(array.get(i))){
+                return i - 1;
+            }
+        }
+        System.out.println("Find Key Failed. Wrong Password. Exiting...");
+        System.exit(0);
+        return 0;
+    }
+
+    public static String getHash(String toHash) throws NoSuchAlgorithmException{
+        MessageDigest md = MessageDigest.getInstance("SHA1");
+        md.reset();
+        byte[] bytes = toHash.getBytes();
+        md.update(bytes);
+        byte[] digest = md.digest();
+        return bytesToHex(digest);
+    }
+}
