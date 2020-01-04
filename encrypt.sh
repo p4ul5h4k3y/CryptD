5d193fb846d771fdac00391641d59d64b471529b
pschake34 <pschake34@charlottesvilleschools.org>
2020-01-04 13:18:56 -0500

    Finished startup bash script

--- encrypt.sh
+++ encrypt.sh
@@ -1,22 +1,8 @@
-#!/bin/bash
-
+#!/usr/bin/bash
 echo Initializing the encryption engines
 sleep 2
 echo Vroom Vroom...
 sleep 1
 echo Ready for liftoff
 
-if [ $# -lt 2 ]; then
-  print_usage;
-fi
-
-function print_usage() {
-  echo """Usage: encrypt [OPTION]... [ARGS]...
-      -d                                set mode to decrypt
-      -e                                set mode to encrypt
-      -g                                generate keypair for encryption
-      -f [path-to-file]                 set path for saving message to (default is current-dir/date_time) when encrypting
-                                            when decrypting it saves the decrypted message to the file
-      -h                                display this info and exit
-  """
-}
+/usr/lib/jvm/java-8-openjdk-amd64/bin/java -classpath /usr/lib/jvm/java-8-openjdk-amd64/jre/lib/bcprov-jdk15on-164.jar:/home/user/Desktop/Programming/Java/Cryptography/out/production/Cryptography Encrypt "$*"
