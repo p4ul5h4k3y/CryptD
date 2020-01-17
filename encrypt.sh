1c682decd409eeab9b6afc20ea1388fd853d9fac
p4ul5h4k3y <psdeveloper@mailfence.com>
2020-01-17 18:33:33 -0500

    Add support for generating keypair flag and add credits

--- encrypt.sh
+++ encrypt.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/bash
-echo Initializing the encryption engines
-sleep 2
-echo Vroom Vroom...
-sleep 1
-echo Ready for liftoff
 
-/usr/lib/jvm/java-8-openjdk-amd64/bin/java -classpath /usr/lib/jvm/java-8-openjdk-amd64/jre/lib/bcprov-jdk15on-164.jar:/home/user/Desktop/Programming/Java/Cryptography/out/production/Cryptography Encrypt "$*"
+#Written by Paul Schakel
+#This bash script runs the encryption program
+echo $1
+if [ $1 == "-g" ]; then
+    /usr/lib/jvm/java-8-openjdk-amd64/bin/java -classpath /usr/lib/jvm/java-8-openjdk-amd64/jre/lib/bcprov-jdk15on-164.jar:/home/user/Desktop/Programming/Java/Cryptography/out/production/Cryptography CreateKeypair $*
+else
+    /usr/lib/jvm/java-8-openjdk-amd64/bin/java -classpath /usr/lib/jvm/java-8-openjdk-amd64/jre/lib/bcprov-jdk15on-164.jar:/home/user/Desktop/Programming/Java/Cryptography/out/production/Cryptography Encrypt $*
+fi
