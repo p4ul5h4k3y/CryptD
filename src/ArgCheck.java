8a711c7a9216494c05aa598e1d5f2bd9d02a453e
p4ul5h4k3y <psdeveloper@mailfence.com>
2020-01-17 02:07:37 -0500

    Add credits and specific functions to check for arguments

--- src/ArgCheck.java
+++ src/ArgCheck.java
@@ -1,19 +1,75 @@
+//Written by Paul Schakel
+//This class provides functions and classes useful for checking arguments in the main java class
+
+
 public class ArgCheck {
-    public ArgCheck(String[] newArgList){
+    public ArgCheck(String[] newArgList) {
         argList = newArgList;
     }
 
     public String[] argList;
 
-    public boolean check(String toCheck) {
+    public boolean checkIfPresent(String toCheck) {
         int i = 0;
 
-        for (String arg = argList[i]; i <= argList.length; i++) {
+        while  (i < argList.length) {
+            String arg = argList[i];
             if (toCheck.equals(arg)) {
                 return true;
             }
+            i++;
         }
 
         return false;
+    }
+
+    public BoolAndPos checkIfDecrypt(String[] toCheck) {
+        for (int i = 0; i < toCheck.length; i++) {
+            String arg = toCheck[i];
+            if (arg.equals("-d")) {
+                return new BoolAndPos(true, i);
+            }
+        }
+        return new BoolAndPos(false, 69420);
+    }
+
+    public boolean checkIfEncrypt(String[] toCheck) {
+        for (int i = 0; i < toCheck.length; i++) {
+            String arg = toCheck[i];
+            if (arg.equals("-e")) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean checkIfHelp(String[] toCheck) {
+        for (int i = 0; i < toCheck.length; i++) {
+            String arg = toCheck[i];
+            if (arg.equals("-h")) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public BoolAndPos checkIfPath(String[] toCheck) {
+        for (int i = 0; i < toCheck.length; i++) {
+            String arg = toCheck[i];
+            if (arg.equals("-f")) {
+                return new BoolAndPos(true, i);
+            }
+        }
+        return new BoolAndPos(false, 69420);
+    }
+
+    public class BoolAndPos {
+        public BoolAndPos(boolean newBool, int newPos) {
+            bool = newBool;
+            pos = newPos;
+        }
+
+        public boolean bool;
+        public int pos;
     }
 }
