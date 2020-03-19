43ba9e87807326227aecfb47eb7a4492320e8611
pschake34 <pschake34@charlottesvilleschools.org>
2020-03-19 01:28:07 -0400

    Add another datatype

---
+++ src/BoolAndFilename.java
@@ -0,0 +1,8 @@
+public class BoolAndFilename{
+    public BoolAndFilename(boolean newBool, String newFilename) {
+        bool = newBool;
+        filename = newFilename;
+    }
+    public boolean bool;
+    public String filename;
+}
