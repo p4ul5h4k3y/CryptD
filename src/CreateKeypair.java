948b6175c079956b72fd8cbcdcd1d0864dd46e68
p4ul5h4k3y <psdeveloper@mailfence.com>
2020-03-20 03:03:37 -0400

    Rewrite certificate generation system so it doesn't use deprecated classes

--- src/CreateKeypair.java
+++ src/CreateKeypair.java
@@ -2,9 +2,19 @@
 //This class creates and stores the keypair for encryption and saves the path to the public and private keys in configuration.conf
 
 
+import org.bouncycastle.asn1.x500.X500Name;
+import org.bouncycastle.asn1.x509.AlgorithmIdentifier;
+import org.bouncycastle.asn1.x509.SubjectPublicKeyInfo;
+import org.bouncycastle.cert.X509CertificateHolder;
+import org.bouncycastle.cert.jcajce.JcaX509CertificateConverter;
+import org.bouncycastle.crypto.params.AsymmetricKeyParameter;
+import org.bouncycastle.crypto.util.PrivateKeyFactory;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
-import org.bouncycastle.x509.X509V1CertificateGenerator;
-import javax.security.auth.x500.X500Principal;
+import org.bouncycastle.cert.X509v1CertificateBuilder;
+import org.bouncycastle.operator.ContentSigner;
+import org.bouncycastle.operator.DefaultDigestAlgorithmIdentifierFinder;
+import org.bouncycastle.operator.DefaultSignatureAlgorithmIdentifierFinder;
+import org.bouncycastle.operator.bc.BcRSAContentSignerBuilder;
 import java.io.*;
 import java.math.BigInteger;
 import java.security.*;
@@ -20,10 +30,10 @@ public class CreateKeypair {
     public static void main(String[] args) throws NoSuchAlgorithmException, NoSuchProviderException {
         Security.addProvider(new BouncyCastleProvider());
 
-        String path = parseArgs(args, "-p");
+        String path = parseArgs(args, "-g");
         if (path.equals("NO ARGS")) {
             System.out.println("E: Path argument necessary to create keypair");
-            System.exit(0);
+            System.exit(1);
         }
         String pass1 = getPassword();
         genAndStoreKeys(pass1, "/home/user/Desktop/Programming/Java/Cryptography", path);
@@ -82,22 +92,28 @@ public class CreateKeypair {
 
     public static X509Certificate[] genCertChain(PublicKey pubKey, PrivateKey privKey) {
         try {
-            X509V1CertificateGenerator certGen = new X509V1CertificateGenerator();
-
-            X500Principal dnName = new X500Principal("CN=ROOT");
+            SubjectPublicKeyInfo subPubKeyInfo = SubjectPublicKeyInfo.getInstance(pubKey.getEncoded());
+            X500Name dnName = new X500Name("CN=ROOT");
             Date validityBeginDate = new Date(System.currentTimeMillis() - 24 * 60 * 60 * 1000);
             Date validityEndDate = new Date(System.currentTimeMillis() + 3 * 365 * 24 * 60 * 60 * 1000);
 
-            certGen.setSerialNumber(BigInteger.valueOf(System.currentTimeMillis()));
-            certGen.setSubjectDN(dnName);
-            certGen.setIssuerDN(dnName);
-            certGen.setNotBefore(validityBeginDate);
-            certGen.setNotAfter(validityEndDate);
-            certGen.setPublicKey(pubKey);
-            certGen.setSignatureAlgorithm("SHA256WithRSAEncryption");
+            X509v1CertificateBuilder certBuilder = new X509v1CertificateBuilder(
+                    dnName,
+                    BigInteger.valueOf(System.currentTimeMillis()),
+                    validityBeginDate,
+                    validityEndDate,
+                    dnName,
+                    subPubKeyInfo
+            );
 
-            X509Certificate rootCertificate = certGen.generate(privKey, "BC");
-            X509Certificate middleCertificate = certGen.generate(privKey, "BC");
+            AlgorithmIdentifier sigAlgId = new DefaultSignatureAlgorithmIdentifierFinder().find("SHA256WithRSAEncryption");
+            AlgorithmIdentifier digAlgId = new DefaultDigestAlgorithmIdentifierFinder().find(sigAlgId);
+            ContentSigner signer = new BcRSAContentSignerBuilder(sigAlgId, digAlgId).build(PrivateKeyFactory.createKey(privKey.getEncoded()));
+
+            X509CertificateHolder holder = certBuilder.build(signer);
+
+            X509Certificate rootCertificate = new JcaX509CertificateConverter().getCertificate(holder);
+            X509Certificate middleCertificate = new JcaX509CertificateConverter().getCertificate(holder);
 
             X509Certificate[] chain = new X509Certificate[2];
             chain[0] = rootCertificate;
