27d226c0ffb6f2cc9603610ea9cf5acc2bb603a5
pschake34 <pschake34@charlottesvilleschools.org>
2020-03-20 03:01:27 -0400

    Add another bouncycastle jar to classpath and remove debug line

--- encrypt.sh
+++ encrypt.sh
@@ -2,9 +2,9 @@
 
 #Written by Paul Schakel
 #This bash script runs the encryption program
-echo $1
+
 if [ $1 == "-g" ]; then
-    /usr/lib/jvm/java-8-openjdk-amd64/bin/java -classpath /usr/lib/jvm/java-8-openjdk-amd64/jre/lib/bcprov-jdk15on-164.jar:/home/user/Desktop/Programming/Java/Cryptography/out/production/Cryptography CreateKeypair $*
+    /usr/lib/jvm/java-8-openjdk-amd64/bin/java -classpath /usr/lib/jvm/java-8-openjdk-amd64/jre/lib/bcprov-jdk15on-164.jar:/usr/lib/jvm/java-8-openjdk-amd64/jre/lib/ext/bcpkix-jdk15on-164.jar:/home/user/Desktop/Programming/Java/Cryptography/out/production/Cryptography CreateKeypair $*
 else
-    /usr/lib/jvm/java-8-openjdk-amd64/bin/java -classpath /usr/lib/jvm/java-8-openjdk-amd64/jre/lib/bcprov-jdk15on-164.jar:/home/user/Desktop/Programming/Java/Cryptography/out/production/Cryptography CryptD $*
+    /usr/lib/jvm/java-8-openjdk-amd64/bin/java -classpath /usr/lib/jvm/java-8-openjdk-amd64/jre/lib/bcprov-jdk15on-164.jar:/usr/lib/jvm/java-8-openjdk-amd64/jre/lib/ext/bcpkix-jdk15on-164.jar:/home/user/Desktop/Programming/Java/Cryptography/out/production/Cryptography CryptD $*
 fi
