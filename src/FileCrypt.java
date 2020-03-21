fb4b3b2a724e9dd95c7d1b20d8ac052907ef2916
p4ul5h4k3y <psdeveloper@mailfence.com>
2020-03-21 03:38:11 -0400

    Added FileCrypt which encrypts files

---
+++ src/FileCrypt.java
@@ -0,0 +1,85 @@
+//Written by Paul Schakel
+//This class is an extension of TextCrypt which provides support for the encryption of files
+
+
+import datatypes.*;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import java.io.*;
+import java.nio.file.Files;
+import java.security.*;
+
+public class FileCrypt extends TextCrypt {
+    public FileCrypt(BoolAndFilename isEncrypt, BoolAndFilename hasFilePath, SecretKey currentSessionKey) {
+        super(isEncrypt, hasFilePath, currentSessionKey);
+        Security.addProvider(new BouncyCastleProvider());
+
+        if (isEncrypt.bool) {
+            PublicKey key = (PublicKey) getKey(false);
+            File toEncrypt = new File(isEncrypt.filename);
+            byte[] encrypted = fileEncrypt(toEncrypt, currentSessionKey);
+            if (hasFilePath.bool) {
+                String filename = storeTextAndKey(new TextAndKey(bytesToHex(encrypted), currentSessionKey), key, hasFilePath.filename);
+                System.out.println("\nSaved encrypted text to file : " + filename);
+            } else {
+                String filename = storeTextAndKey(new TextAndKey(bytesToHex(encrypted), currentSessionKey), key, toEncrypt.getName() + ".crypt");
+                System.out.println("\nSaved encrypted text to file : " + filename);
+            }
+        } else {
+            try {
+                PrivateKey key = (PrivateKey) getKey(true);
+                TextAndKey cryptTextAndKey = TextCrypt.getTextAndKey(isEncrypt.filename, key);
+                assert cryptTextAndKey != null;
+                SecretKey oldSessionKey = cryptTextAndKey.sessionKey;
+                byte[] plainFileBytes = fileDecrypt(TextCrypt.hexToBytes(cryptTextAndKey.text), oldSessionKey);
+                if (hasFilePath.bool) {
+                    FileOutputStream fos = new FileOutputStream(hasFilePath.filename);
+                    assert plainFileBytes != null;
+                    fos.write(plainFileBytes);
+                    fos.close();
+                    System.out.println("Decrypted File saved at: " + hasFilePath.filename);
+                } else {
+                    String[] filename = new File(isEncrypt.filename).getName().split("\\.(?=[^\\.]+$)");
+                    FileOutputStream fos = new FileOutputStream(filename[0]);
+                    assert plainFileBytes != null;
+                    fos.write(plainFileBytes);
+                    fos.close();
+                    System.out.println("Decrypted File saved at: " + filename[0]);
+                }
+            } catch (ArrayIndexOutOfBoundsException ex) {
+                System.out.println("E: Path to .crypt file required");
+                System.exit(1);
+            } catch (IOException ex) {
+                System.out.println("E: Error while writing output to file");
+                System.exit(1);
+            }
+        }
+    }
+
+    public static byte[] fileEncrypt(File plainFile, SecretKey cryptKey) {
+        try {
+            byte[] fileContent = Files.readAllBytes(plainFile.toPath());
+
+            Cipher aesCipher = Cipher.getInstance("AES/ECB/PKCS7Padding", "BC");
+            aesCipher.init(Cipher.ENCRYPT_MODE, cryptKey);
+            return aesCipher.doFinal(fileContent);
+        } catch (Exception ex) {
+            System.out.println("E: Error occurred during file encryption");
+            System.exit(1);
+            return null;
+        }
+    }
+
+    public static byte[] fileDecrypt(byte[] encryptedFile, SecretKey cryptKey) {         //takes the encrypted byte array and decrypts it with the secret key provided
+        try {
+            Cipher aesCipher = Cipher.getInstance("AES/ECB/PKCS7Padding", "BC");
+            aesCipher.init(Cipher.DECRYPT_MODE, cryptKey);
+            return aesCipher.doFinal(encryptedFile);
+        } catch (Exception ex) {
+            System.out.println("E: Error occurred during decryption");
+            System.exit(1);
+            return null;
+        }
+    }
+}
