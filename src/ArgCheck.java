abdc85ae3fc1aa8f89b103a6f706ba96b3452104
p4ul5h4k3y <psdeveloper@mailfence.com>
2020-03-23 01:33:59 -0400

    Add support for file encryption checks and updated the usage

--- src/ArgCheck.java
+++ src/ArgCheck.java
@@ -29,18 +29,21 @@ public class ArgCheck {
         for (int i = 0; i < toCheck.length; i++) {
             String arg = toCheck[i];
             if (arg.equals("-d")) {
+                if (toCheck.length < 2) {
+                    System.out.println("E: No path to file specified");
+                    System.exit(1);
+                }
                 return new BoolAndPos(false, i);
             }
             else if (arg.equals("-e")) {
-                return new BoolAndPos(true, 0);
+                return new BoolAndPos(true, -1);
             }
         }
         return null;
     }
 
     public boolean checkIfHelp(String[] toCheck) {
-        for (int i = 0; i < toCheck.length; i++) {
-            String arg = toCheck[i];
+        for (String arg : toCheck) {
             if (arg.equals("-h")) {
                 return true;
             }
@@ -51,19 +54,40 @@ public class ArgCheck {
     public BoolAndPos checkIfPath(String[] toCheck) {
         for (int i = 0; i < toCheck.length; i++) {
             String arg = toCheck[i];
+            if (arg.equals("-p")) {
+                return new BoolAndPos(true, i);
+            }
+        }
+        return new BoolAndPos(false, -1);
+    }
+
+    public BoolAndPos checkIfFile(String[] toCheck) {
+        for (int i = 0; i < toCheck.length; i++) {
+            String arg = toCheck[i];
             if (arg.equals("-f")) {
                 return new BoolAndPos(true, i);
             }
         }
-        return new BoolAndPos(false, 0);
+        return new BoolAndPos(false, -1);
+    }
+
+    public boolean checkIfRecurs(String[] toCheck) {
+        for (String arg : toCheck) {
+            if (arg.equals("-r")) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public static void printUsage() {
         System.out.println("Usage: encrypt [OPTION]... [ARGS]... \n" +
                 "      -d [path-to-encrypted-text]       set mode to decrypt\n" +
                 "      -e                                set mode to encrypt\n" +
+                "      -f [path-to-file]                 encrypt a specified file\n" +
+                "      -r                                used to specify recursive encryption of a directory\n" +
                 "      -g                                generate keypair for encryption\n" +
-                "      -f [path-to-file]                 set path for saving message to (default is current-dir/date_time) when encrypting\n" +
+                "      -p [path-to-file]                 set path for saving message to (default is current-dir/date_time) when encrypting\n" +
                 "                                            when decrypting it saves the decrypted message to the file\n" +
                 "      -h                                display this info and exit");
     }
