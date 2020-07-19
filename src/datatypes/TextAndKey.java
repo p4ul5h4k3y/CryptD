134b807b06e7f153878d60336c3f431b99ae46e4
pschake34 <pschake34@charlottesvilleschools.org>
2020-07-19 11:09:38 -0400

    Add support for metadata

--- src/datatypes/TextAndKey.java
+++ src/datatypes/TextAndKey.java
@@ -3,10 +3,12 @@ package datatypes;
 import javax.crypto.SecretKey;
 
 public class TextAndKey {
-    public TextAndKey(String newText, SecretKey newSessionKey) {
+    public TextAndKey(String newText, SecretKey newSessionKey, String newMetadata) {
         text = newText;
         sessionKey = newSessionKey;
+        metadata = newMetadata;
     }
     public String text;
     public SecretKey sessionKey;
+    public String metadata;
 }
