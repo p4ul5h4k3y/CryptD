75d034aafcc270589870bb37bbd9134d9edaee2c
p4ul5h4k3y <psdeveloper@mailfence.com>
2020-11-05 00:50:29 -0500

    Add header comment in datatypes

--- src/main/java/me/p4ul5h4k3y/cryptoparrot/datatypes/BoolAndFilename.java
+++ src/main/java/me/p4ul5h4k3y/cryptoparrot/datatypes/BoolAndFilename.java
@@ -1,5 +1,8 @@
 package me.p4ul5h4k3y.cryptoparrot.datatypes;
 
+//Written by p4ul5h4k3y
+//This class provides a custom datatype which can store: a boolean, and a String (which usually denotes a path to a file)
+
 public class BoolAndFilename{
     public BoolAndFilename(boolean newBool, String newFilename) {
         bool = newBool;
