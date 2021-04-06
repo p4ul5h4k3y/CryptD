7c490ec200b96517be26182072d86e47736b5883
pschake34 <pschake34@charlottesvilleschools.org>
2021-04-06 14:05:20 -0400

    Reorganize file structure of project

--- src/main/java/me/p4ul5h4k3y/cryptoparrot/encryption/FileCrypt.java
+++ src/main/java/me/p4ul5h4k3y/cryptoparrot/encryption/FileCrypt.java
@@ -1,7 +1,8 @@
 package me.p4ul5h4k3y.cryptoparrot;
 
-//Written by Paul Schakel
-//This class is an extension of TextCrypt which provides support for the encryption of files
+//Written by p4ul5h4k3y
+//This class is an extension of Crypt which provides support for the encryption of files. Files and directories are compressed into a zip and then encrypted.
+//In addition, this class contains the methods to unzip the decrypted data.
 
 
 import me.p4ul5h4k3y.cryptoparrot.datatypes.BoolAndFilename;
@@ -15,32 +16,45 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.security.*;
+import java.util.HashMap;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
 
-public class FileCrypt extends me.p4ul5h4k3y.cryptoparrot.Crypt {
-    public FileCrypt(BoolAndFilename isEncrypt, BoolAndFilename hasFilePath, SecretKey currentSessionKey) {
-        super(isEncrypt, hasFilePath);
+public class FileCrypt extends Decrypt {
+
+    static BoolAndFilename isEncrypt;
+    static BoolAndFilename hasFilePath;
+    static SecretKey currentSessionKey;
+
+    public FileCrypt(HashMap<String, Object> args) {
+        super(args);                   //send requests for decryption to Crypt (FileCrypt only encrypts files)
         Security.addProvider(new BouncyCastleProvider());
+
+        isEncrypt = (BoolAndFilename) args.get("-f");
+        hasFilePath = (BoolAndFilename) args.get("-p");
+        currentSessionKey = (SecretKey) args.get("sessionKey");
+    }
+
+    public static void main() {
         File toEncrypt = new File(isEncrypt.filename);
-        if (isEncrypt.bool) {
+        if (isEncrypt.bool) {           //make sure it's supposed to encrypt the data
             PublicKey key = (PublicKey) getKey(false);
             try {
                 ByteArrayOutputStream bos = new ByteArrayOutputStream();
                 ZipOutputStream zipout = new ZipOutputStream(bos);
                 File inputDir = new File(isEncrypt.filename);
                 Path inputPath = Paths.get(isEncrypt.filename);
-                zipFile(inputDir, inputPath.getFileName().toString(), zipout);
-                byte[] bytesToEncrypt = bos.toByteArray();
+                zipFile(inputDir, inputPath.getFileName().toString(), zipout);      //compress files
+                byte[] bytesToEncrypt = bos.toByteArray();      //convert ByteArrayOutputStream which contains compressed files into a ByteArray
                 zipout.close();
                 bos.close();
-                byte[] encrypted = encrypt(bytesToEncrypt, currentSessionKey);
-                if (hasFilePath.bool) {
-                    String filename = storeTextAndKey(new TextAndKey(bytesToHex(encrypted), currentSessionKey, "file/dir"), key, hasFilePath.filename);
+                byte[] encrypted = encrypt(bytesToEncrypt, currentSessionKey);      //encrypt ByteArray
+                if (hasFilePath.bool) {         //decide where to save the encrypted data
+                    String filename = storeTextAndKey(new TextAndKey(bytesToHex(encrypted), currentSessionKey, "file/dir"), key, hasFilePath.filename);     //if user specified where
                     System.out.println("\nSaved encrypted data to file : " + filename);
                 } else {
-                    String filename = storeTextAndKey(new TextAndKey(bytesToHex(encrypted), currentSessionKey, "file/dir"), key, toEncrypt.getName() + ".crypt");
+                    String filename = storeTextAndKey(new TextAndKey(bytesToHex(encrypted), currentSessionKey, "file/dir"), key, toEncrypt.getName() + ".crypt");   //generate location: current_dir/name-of-original-file.crypt
                     System.out.println("\nSaved encrypted data to file : " + filename);
                 }
             } catch (IOException e) {
@@ -48,9 +62,10 @@ public class FileCrypt extends me.p4ul5h4k3y.cryptoparrot.Crypt {
                 System.exit(1);
             }
         }
+
     }
 
-    public static byte[] encrypt(byte[] plainBytes, SecretKey cryptKey) {
+    public static byte[] encrypt(byte[] plainBytes, SecretKey cryptKey) {       //encrypts the byte[] passed to it
         try {
             Cipher aesCipher = Cipher.getInstance("AES/ECB/PKCS7Padding", "BC");
             aesCipher.init(Cipher.ENCRYPT_MODE, cryptKey);
@@ -62,9 +77,9 @@ public class FileCrypt extends me.p4ul5h4k3y.cryptoparrot.Crypt {
         }
     }
 
-    public static void zipFile(File fileToZip, String fileName, ZipOutputStream zipOut) {
+    public static void zipFile(File fileToZip, String fileName, ZipOutputStream zipOut) {       //recursively zips the specified files/directories
         try {
-            if (fileToZip.isDirectory()) {
+            if (fileToZip.isDirectory()) {          //checks if it needs to recursively zip dir
                 if (fileName.endsWith("/")) {
                     zipOut.putNextEntry(new ZipEntry(fileName));
                 } else {
@@ -72,8 +87,8 @@ public class FileCrypt extends me.p4ul5h4k3y.cryptoparrot.Crypt {
                 }
                 zipOut.closeEntry();
                 File[] children = fileToZip.listFiles();
-                for (File childFile : children) {
-                    zipFile(childFile, fileName + "/" + childFile.getName(), zipOut);
+                for (File childFile : children) {           //zips each of the files within the dir
+                    zipFile(childFile, fileName + "/" + childFile.getName(), zipOut);       //creates new instance of this function for each item in dir
                 }
                 return;
             }
@@ -82,7 +97,7 @@ public class FileCrypt extends me.p4ul5h4k3y.cryptoparrot.Crypt {
             zipOut.putNextEntry(zipEntry);
             byte[] bytes = new byte[1024];
             int length;
-            while ((length = fis.read(bytes)) >= 0) {
+            while ((length = fis.read(bytes)) >= 0) {           //writes the new zipEntry to the ZipOutputStream
                 zipOut.write(bytes, 0, length);
             }
             zipOut.closeEntry();        // <--- if you haven't got this little blighter right here then the data in files won't save (at least 10 hrs lost to this guy)
@@ -95,15 +110,15 @@ public class FileCrypt extends me.p4ul5h4k3y.cryptoparrot.Crypt {
         }
     }
 
-    public static void unzipAndWriteDir(byte[] zipped, String destDir) {
+    public static void unzipAndWriteDir(byte[] zipped, String destDir) {            //unzips already-decrypted data and writes it to the specified destination
         try {
             ByteArrayInputStream bis = new ByteArrayInputStream(zipped);
-            ZipInputStream zis = new ZipInputStream(new BufferedInputStream(bis));
+            ZipInputStream zis = new ZipInputStream(new BufferedInputStream(bis));      //convert byte[] to collection of zipEntries
             ZipEntry entry;
             while((entry = zis.getNextEntry()) != null) {
                 Path newFile = Paths.get(destDir, entry.getName());
                 if (!entry.isDirectory()) {
-                    unzipFiles(zis, newFile);
+                    unzipFiles(zis, newFile);           //makes sure entry isn't a directory and sends to unzipFiles() to unzip and write to the filesystem
                 } else {
                     Files.createDirectories(newFile);
                 }
@@ -128,7 +143,7 @@ public class FileCrypt extends me.p4ul5h4k3y.cryptoparrot.Crypt {
             FileOutputStream fos = new FileOutputStream(unzipPath.toAbsolutePath().toString());
             byte[] bytesIn = new byte[1024];
             int read;
-            while ((read = zinput.read(bytesIn, 0, bytesIn.length)) != -1) {
+            while ((read = zinput.read(bytesIn, 0, bytesIn.length)) != -1) {        //write decompressed file to filesystem
                 fos.write(bytesIn, 0, read);
             }
             fos.close();
