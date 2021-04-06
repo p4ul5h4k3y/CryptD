ec2f2dcabb9a2c2d5c76f6c4b9c08c0a73d1f028
pschake34 <pschake34@charlottesvilleschools.org>
2021-04-06 14:11:51 -0400

    Update package info

--- src/main/java/me/p4ul5h4k3y/cryptoparrot/util/datatypes/BoolAndPos.java
+++ src/main/java/me/p4ul5h4k3y/cryptoparrot/util/datatypes/BoolAndPos.java
@@ -1,4 +1,4 @@
-package me.p4ul5h4k3y.cryptoparrot.datatypes;
+package me.p4ul5h4k3y.cryptoparrot.util.datatypes;
 
 //Written by p4ul5h4k3y
 //This class provides a custom datatype which can store: a boolean and an integer (usually denoting an index in an array)
